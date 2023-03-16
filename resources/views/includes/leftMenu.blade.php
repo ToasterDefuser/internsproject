841ba27e3806f160669ac8bfee9ac0d1b24f2c68
Bartosz Szostak <szostakbartosz1122@gmail.com>
2023-03-16 14:26:01 +0100

    Poprawa leftMenu

--- resources/views/includes/leftMenu.blade.php
+++ resources/views/includes/leftMenu.blade.php
@@ -1,4 +1,6 @@
 <div class="leftMenu">
-    <a href="/import"><h3>Import plików</h1></a>
-    <a href="/view"><h3>Podgląd danych</h1></a>
+    <div class="leftMenuBox">
+        <a href="/import"><h3>Import plików</h1></a>
+        <a href="/view"><h3>Podgląd danych</h1></a> 
+    </div>
 </div>
