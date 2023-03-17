ecda575b70d24d89e42ebdd222cb780deada7198
Bartosz Szostak <szostakbartosz1122@gmail.com>
2023-03-17 10:03:36 +0100

    cofniecie zmian

--- database/migrations/2023_03_17_074544_create_invoices_table.php
+++ database/migrations/2023_03_17_074544_create_invoices_table.php
@@ -52,5 +52,10 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('invoices');
+        dropConstrainedForeignId('order_id');
+        dropConstrainedForeignId('delivery_id');
+        dropConstrainedForeignId('buyer_id');
+        dropConstrainedForeignId('seller_id');
+        dropConstrainedForeignId('summary_id');
     }
 };
