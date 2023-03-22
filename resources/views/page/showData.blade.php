fc793655076f0fbaa12c78e0f3d2e9a7c32da168
Bartosz Szostak <szostakbartosz1122@gmail.com>
2023-03-22 11:53:18 +0100

    Poprawka css

--- resources/views/page/showData.blade.php
+++ resources/views/page/showData.blade.php
@@ -1,120 +1,124 @@
 @extends('layouts.default')
 @section('content')
    <div class="table_div">
-    <div>
-        Filtry:
-        <br>
-        <form method="GET" action="">
-        Akronim dostawcy
-        
-        <select name="wartosci" id="" onchange="this.form.submit()">
-            <option value="all"<?php if(isset($_GET['wartosci']) && $_GET['wartosci'] == "all") echo 'selected';?>>wszystkie</option>
-        
-        </form>
+    <div class="filters">
+        <h4>Filtry:</h4>
 
         
 
-            <?php
-                use App\Models\Invoice;
-                use App\Models\Order;
-                use App\Models\Delivery;
-                use App\Models\Seller;
-                use App\Models\Buyer;
-                use App\Models\Item;
-                $selectedBuyerName= "all";
-                $selectedInvoiceNumber = "all";
-                $selectedEAN = "all";
-                //dodawanie opcji select dla buyer
-                //pętla sprawdza czy jakaś wartość została już przedtem wybrana i nadaje jej 'selected', inaczej
-                //po każdej zmianie filtry sie restują i można wybrać tylko 1 filtr
-                $buyers = Buyer::all();
-                $selectedValue = false;
-                foreach($buyers as $buyer){
-                    if(isset($_GET['wartosci']) && urldecode($_GET['wartosci']) == $buyer->Name){
-                        $selectedValue = true;
-                    }
-                    if($selectedValue){
-                        echo '<option value='.urlencode($buyer->Name)." ".'selected'.'>'.$buyer->Name.'</option>';
-                    }else{
-                        echo '<option value='.urlencode($buyer->Name).'>'.$buyer->Name.'</option>';
-                    }
-                    $selectedValue = false;
-                    
-                }
-                if(isset($_GET["wartosci"])){
-                    $selectedBuyerName = urldecode($_GET["wartosci"]);
-                    if($selectedBuyerName == ""){
-                        $selectedBuyerName = "all";
-                    }
-                }
-            ?>
-
-        </select>
-
-        <form method="GET" action = "">
-        Nr faktury
-        <br>
-        <select name="faktura" id="" onchange="this.form.submit()">
-        <option value="all"<?php if(isset($_GET['faktura']) && $_GET['faktura'] == "all") echo 'selected';?>>wszystkie</option>
-        </form>
-
-        <?php
-        //dodawanie opcji select dla nr. faktury
-        $invoiceForm = Invoice::select('InvoiceNumber')->distinct()->get();
-        $selectedValue = false;
-                foreach($invoiceForm as $invoice){
-                    if(isset($_GET['faktura']) && urldecode($_GET['faktura']) == $invoice->InvoiceNumber){
-                        $selectedValue = true;
-                    }
-                    if($selectedValue){
-                        echo '<option value='.urlencode($invoice->InvoiceNumber)." ".'selected'.'>'.$invoice->InvoiceNumber.'</option>';
-                    }else{
-                        echo '<option value='.urlencode($invoice->InvoiceNumber).'>'.$invoice->InvoiceNumber.'</option>';
-                    }
-                    $selectedValue = false;
-                    
-                }
-        if(isset($_GET["faktura"])){
-                $selectedInvoiceNumber = urldecode($_GET["faktura"]);
-                if($selectedInvoiceNumber == ""){
-                    $selectedInvoiceNumber = "all";
-                }
-            }
-        ?>
-        </select>
-
-        
-        <form method="GET" action="">
-        kod towaru        
-        <select name="towar" id="" onchange="this.form.submit()">
-            <option value="all"<?php if(isset($_GET['towar']) && $_GET['towar'] == "all") echo 'selected';?>>wszystkie</option>
-        </form>
-
-        <?php
-        //dodawanie opcji select dla ean
-        $kodTowaru = Item::select('EAN')->distinct()->get();
-        $selectedValue = false;
-        foreach($kodTowaru as $kod){
-            if(isset($_GET['towar']) && urldecode($_GET['towar']) == $kod->EAN){
-                $selectedValue = true;
-            }
-            if($selectedValue){
-                echo '<option value='.urlencode($kod->EAN)." ".'selected'.'>'.$kod->EAN.'</option>';
-            }else{
-                echo '<option value='.urlencode($kod->EAN).'>'.$kod->EAN.'</option>';
-            }
-            $selectedValue = false;
+            <form method="GET" action="" class="filer_form">
+            Akronim dostawcy
             
-        }
-if(isset($_GET["towar"])){
-        $selectedEAN = urldecode($_GET["towar"]);
-        if($selectedEAN == ""){
-            $selectedEAN = "all";
-        }
-    }
-        ?>
-        </select>
+            <select name="wartosci" id="" onchange="this.form.submit()">
+                <option value="all"<?php if(isset($_GET['wartosci']) && $_GET['wartosci'] == "all") echo 'selected';?>>wszystkie</option>
+            
+            
+    
+            
+    
+                <?php
+                    use App\Models\Invoice;
+                    use App\Models\Order;
+                    use App\Models\Delivery;
+                    use App\Models\Seller;
+                    use App\Models\Buyer;
+                    use App\Models\Item;
+                    $selectedBuyerName= "all";
+                    $selectedInvoiceNumber = "all";
+                    $selectedEAN = "all";
+                    //dodawanie opcji select dla buyer
+                    //pętla sprawdza czy jakaś wartość została już przedtem wybrana i nadaje jej 'selected', inaczej
+                    //po każdej zmianie filtry sie restują i można wybrać tylko 1 filtr
+                    $buyers = Buyer::all();
+                    $selectedValue = false;
+                    foreach($buyers as $buyer){
+                        if(isset($_GET['wartosci']) && urldecode($_GET['wartosci']) == $buyer->Name){
+                            $selectedValue = true;
+                        }
+                        if($selectedValue){
+                            echo '<option value='.urlencode($buyer->Name)." ".'selected'.'>'.$buyer->Name.'</option>';
+                        }else{
+                            echo '<option value='.urlencode($buyer->Name).'>'.$buyer->Name.'</option>';
+                        }
+                        $selectedValue = false;
+                        
+                    }
+                    if(isset($_GET["wartosci"])){
+                        $selectedBuyerName = urldecode($_GET["wartosci"]);
+                        if($selectedBuyerName == ""){
+                            $selectedBuyerName = "all";
+                        }
+                    }
+                ?>
+    
+            </select>
+    
 
+            Nr faktury
+            <br>
+            <select name="faktura" id="" onchange="this.form.submit()">
+            <option value="all"<?php if(isset($_GET['faktura']) && $_GET['faktura'] == "all") echo 'selected';?>>wszystkie</option>
+
+    
+            <?php
+                //dodawanie opcji select dla nr. faktury
+                $invoiceForm = Invoice::select('InvoiceNumber')->distinct()->get();
+                $selectedValue = false;
+                        foreach($invoiceForm as $invoice){
+                            if(isset($_GET['faktura']) && urldecode($_GET['faktura']) == $invoice->InvoiceNumber){
+                                $selectedValue = true;
+                            }
+                            if($selectedValue){
+                                echo '<option value='.urlencode($invoice->InvoiceNumber)." ".'selected'.'>'.$invoice->InvoiceNumber.'</option>';
+                            }else{
+                                echo '<option value='.urlencode($invoice->InvoiceNumber).'>'.$invoice->InvoiceNumber.'</option>';
+                            }
+                            $selectedValue = false;
+                            
+                        }
+                if(isset($_GET["faktura"])){
+                        $selectedInvoiceNumber = urldecode($_GET["faktura"]);
+                        if($selectedInvoiceNumber == ""){
+                            $selectedInvoiceNumber = "all";
+                        }
+                    }
+            ?>
+            </select>
+    
+            
+        
+            kod towaru        
+            <select name="towar" id="" onchange="this.form.submit()">
+                <option value="all"<?php if(isset($_GET['towar']) && $_GET['towar'] == "all") echo 'selected';?>>wszystkie</option>
+ 
+    
+            <?php
+            //dodawanie opcji select dla ean
+            $kodTowaru = Item::select('EAN')->distinct()->get();
+            $selectedValue = false;
+            foreach($kodTowaru as $kod){
+                if(isset($_GET['towar']) && urldecode($_GET['towar']) == $kod->EAN){
+                    $selectedValue = true;
+                }
+                if($selectedValue){
+                    echo '<option value='.urlencode($kod->EAN)." ".'selected'.'>'.$kod->EAN.'</option>';
+                }else{
+                    echo '<option value='.urlencode($kod->EAN).'>'.$kod->EAN.'</option>';
+                }
+                $selectedValue = false;
+                
+            }
+    if(isset($_GET["towar"])){
+            $selectedEAN = urldecode($_GET["towar"]);
+            if($selectedEAN == ""){
+                $selectedEAN = "all";
+            }
+        }
+            ?>
+            </select>
+        
+        
+            </form>
     </div>
     <table>
         <tr>
